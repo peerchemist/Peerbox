f27e3fb18573f73e394f5935d4994e1fdc550290
peerchemist <dx.1178@gmail.com>
2014-07-02 17:53:28 +0200

    small tweak

--- peerbox-info/peerbox-info.sh
+++ peerbox-info/peerbox-info.sh
@@ -4,9 +4,9 @@ txtylw=$(tput setaf 3) # Coin Yellow
 txtrst=$(tput sgr0) # Resets Text
 clear
 echo
-echo ${txtgrn}PeerBox Node Info${txtrst}. ${txtylw}
+echo ${txtgrn}PeerBox Node Info${txtrst}: ${txtylw}
 ppcoind getinfo | grep -vE 'ip|proxy|{|}'
 echo ${txtrst}
-echo ${txtgrn}Raspberry Pi Info${txtrst}. ${txtylw}
+echo ${txtgrn}Raspberry Pi Info${txtrst}: ${txtylw}
 grep 'Hardware\|Serial' /proc/cpuinfo
 echo ${txtrst}
