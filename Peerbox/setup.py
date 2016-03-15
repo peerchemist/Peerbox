9e7dd06b1e6a9a0f5830ecc02565e534f3afde6c
Peerchemist <dx.1178@gmail.com>
2016-03-15 09:11:27 +0100

    redesign of repository, added both python and debian package files

---
+++ Peerbox/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='peerbox',
+      version='0.5.0',
+      description='Peerbox control scripts',
+      url='https://github.com/peerchemist/Peerbox',
+      author='Peerchemist',
+      author_email='peerchemist@protonmail.ch',
+      license='GLP',
+      packages=['peerbox'],
+      install_requires=["requests", "sh"],
+      scripts=['bin/peerbox'],
+      zip_safe=False)
