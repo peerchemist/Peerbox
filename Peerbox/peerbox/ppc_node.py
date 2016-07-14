c87e3775b22f20d88e45d06c37e4ea6ae3a83c70
Peerchemist <dx.1178@gmail.com>
2016-07-14 11:11:46 +0000

    release 0.5.6.2

--- Peerbox/peerbox/ppc_node.py
+++ Peerbox/peerbox/ppc_node.py
@@ -24,10 +24,13 @@ from jsonrpc import Server
 
 class Node:
     
-    def __init__(self):
+    def __init__(self, testnet):
         self.username = ""
         self.password = ""
-        self.testnet = False
+        if testnet == True:
+            self.testnet = True
+        else:
+            self.testnet = False
         self.testnet_check()
         self.userpass()
 
@@ -41,7 +44,6 @@ class Node:
 
     def userpass(self):
         '''Reads .ppcoin/ppcoin.conf file for username/password'''
-
         with open('/home/{0}/.ppcoin/ppcoin.conf'.format(getpass.getuser()), 'r') as conf:
             for line in conf:
                 if line.startswith('rpcuser'):
@@ -57,7 +59,7 @@ class Node:
                         #self.testnet = True
                         return True
 
-    def walletpassphrase(self, passphrase, timeout=999999, mint_only=True):
+    def walletpassphrase(self, passphrase, timeout=99999999, mint_only=True):
         '''used to unlock wallet for minting'''
         return self.node.walletpassphrase(passphrase, timeout, mint_only)
 
@@ -73,9 +75,13 @@ class Node:
         '''retrieve block hash'''
         return self.node.getblockhash(block)
 
-    def getbalance(self):
-        '''retrieve balance'''
-        return self.node.getbalance()
+    def gettransaction(self, txid):
+        '''get transaction info'''
+        return self.node.gettransaction(txid)
+
+    def getbalance(self, account=""):
+        '''retrieve balance, If [account] is specified, returns the balance in the account. '''
+        return self.node.getbalance(account)
 
     def getdifficulty(self):
         '''Get PoS/PoW difficulty'''
@@ -92,13 +98,19 @@ class Node:
         '''can be used to list asociated addresses'''
         return self.node.getaddressesbyaccount(account)
 
-    def createnewaddress(self):
-        return self.node.createnewaddress()
+    def getnewaddress(self, label=""):
+        return self.node.getnewaddress(label)
 
     def sendtoaddress(self, recv_addr, amount, comment=""):
         '''send ammount to address, with optional comment. Returns txid.
         sendtoaddress(ADDRESS, AMMOUNT, COMMENT)'''
         return self.node.sendtoaddress(recv_addr, amount, comment)
+
+    def sendmany(self, recv_dict, account="", comment=""):
+        '''send outgoing tx to many addresses, input is dict of addr:coins, 
+        returns txid'''
+        #{"addr1":#coin,"addr2":#coin,"addr3":#coin...}
+        return self.node.sendmany(account, recv_dict, comment)
 
     def getconnectioncount(self):
         '''Get number of active connections'''
@@ -116,15 +128,39 @@ class Node:
         '''returns raw mempool'''
         return self.node.getrawmempool()
 
-    def listtransactions(self):
+    def listtransactions(self, account="", many=999, since=0):
         '''list all transactions associated with this wallet'''
-        return self.node.listtransactions()
+        return self.node.listtransactions(account, many, since)
 
     def listreceivedbyaddress(self, minconf=0, includeempty=True):
         '''get list of all accounts in the wallet'''
         return self.node.listreceivedbyaddress(minconf, includeempty)
 
-    def listunspent(self):
+    def listunspent(self, minconf=1, maxconf=999999): #listunspent 0 999999 '["1BxtgEa8UcrMzVZaW32zVyJh4Sg4KGFzxA"]'
         '''list only unspent UTXO's'''
-        return self.node.listunspent()
+        return self.node.listunspent(minconf, maxconf)
+
+    def dumpprivkey(self, addr):
+        '''returns privkey of address'''
+        return self.node.dumpprivkey(addr)
+
+    def createrawtransaction(self, inputs, outputs):
+        '''[{"txid":input_txid,"vout":0}, ...], {recv_addr: amount, change: amount, ...}'''
+        if type(inputs) != list:
+            raise ValueError('inputs variable must be a list!')
+        if type(outputs) != dict:
+            raise ValueError('outputs variable must be a dictionary!')
+        return self.node.createrawtransaction(inputs, outputs)
+    
+    def decoderawtransaction(self, txhash):
+        '''dump the transaction draft'''
+        return self.node.decoderawtransaction(txhash)
+
+    def signrawtransaction(self, rawtxhash):
+        '''signrawtransaction with privkey, returns status and rawtxhash'''
+        return self.node.signrawtransaction(rawtxhash)
+
+    def sendrawtransaction(self, signed_rawtxhash):
+        '''sends raw transaction, returns txid'''
+        return self.node.sendrawtransaction(signed_rawtxhash)
 
