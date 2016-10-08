9a37229b329d10fbbc92f707ebb783ec9166499f
Peerchemist <dx.1178@gmail.com>
2016-10-08 10:41:20 +0200

    v0.6.1

--- Peerbox/setup.py
+++ Peerbox/setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
 setup(name='peerbox',
-      version='0.5.7',
+      version='0.6.1',
       description='Peerbox control scripts',
       url='https://github.com/peerchemist/Peerbox',
       author='Peerchemist',
       author_email='peerchemist@protonmail.ch',
       license='GLP',
-      packages=['peerbox'],
-      install_requires=["requests", "sh"],
       scripts=['bin/peerbox'],
       zip_safe=False)
