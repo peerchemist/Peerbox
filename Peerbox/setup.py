e5ece10e33f280855f79527a09ce17b58241b9dc
Peerchemist <dx.1178@gmail.com>
2016-10-11 12:25:17 +0200

    bump to v0.6.3

--- Peerbox/setup.py
+++ Peerbox/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='peerbox',
-      version='0.6.2',
+      version='0.6.3',
       description='Peerbox control scripts',
       url='https://github.com/peerchemist/Peerbox',
       author='Peerchemist',
